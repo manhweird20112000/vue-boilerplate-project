85f50161a45f16c2d66176019075451f40e81f29
manhweird20112000 <dinhvanmanh20112000@gmail.com>
2021-12-26 20:49:06 +0700

    update

--- src/app/config/url.js
+++ src/app/config/url.js
@@ -1,3 +1,4 @@
 export const URL_APP = process.env.VUE_APP_URL;
-export const URl_API = process.env.VUE_APP_API;
+export const URl_API =
+	process.env.VUE_APP_API || 'https://rickandmortyapi.com/';
 export const URL_SOCKET = process.env.VUE_APP_SOCKET;
